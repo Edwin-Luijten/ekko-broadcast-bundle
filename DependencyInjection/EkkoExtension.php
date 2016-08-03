72bb1a818d2e415b6e0ebf8fdd3135b31f9688f4
Edwin Luijten <edwin@mediamonks.com>
2016-08-03 17:28:36 +0200

    remove old files

--- DependencyInjection/EkkoExtension.php
+++ DependencyInjection/EkkoExtension.php
@@ -2,11 +2,8 @@
 
 namespace EdwinLuijten\EkkoBundle\DependencyInjection;
 
-use EdwinLuijten\Ekko\Broadcasters\BroadcasterInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Definition;
-use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -25,38 +22,7 @@ class EkkoExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
 
-        $config = $this->processConfiguration(new Configuration(), $configs);
+        $container->addCompilerPass(new BroadcasterPass());
 
-        $this->configureBroadcastManager($config['broadcaster'], $container);
-        $this->configureBroadcastManager($config['broadcaster'], $container);
-    }
-
-    /**
-     * @param array $config
-     * @param ContainerBuilder $container
-     */
-    private function configureBroadcastManager(array $config, ContainerBuilder $container)
-    {
-        $broadcastManager = $container->getDefinition('broadcast.manager');
-        $broadcastManager->addArgument($config['connections']);
-        $broadcastManager->addMethodCall('setDefaultBroadcaster', [$config['default']]);
-
-        $connection = new Definition($broadcastManager->getClass(), ['default']);
-        $connection->setFactory([
-            new Reference('broadcast.manager'),
-            'connection',
-        ]);
-        $container->setDefinition('broadcast.default', $connection);
-
-        // Add connections
-        foreach ($config['connections'] as $name => $broadcaster) {
-            $connection = new Definition($broadcastManager->getClass(), [$name]);
-            $connection->setFactory([
-                new Reference('broadcast.manager'),
-                'connection',
-            ]);
-
-            $container->setDefinition('broadcast.' . $name, $connection);
-        }
     }
 }
