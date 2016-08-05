52c567052633e341445d21e999d7466d34f90efa
Edwin Luijten <edwin@mediamonks.com>
2016-08-05 14:26:24 +0200

    Bundle unit test

--- tests/src/EkkoBundleTest.php
+++ tests/src/EkkoBundleTest.php
@@ -4,12 +4,14 @@ namespace EdwinLuijten\EkkoBundle\Tests;
 
 use EdwinLuijten\EkkoBundle\DependencyInjection\EkkoExtension;
 use EdwinLuijten\EkkoBundle\EkkoBundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class EkkoBundleTest extends \PHPUnit_Framework_TestCase {
 
     public function testGetContainerExtension()
     {
         $bundle = new EkkoBundle();
+        $bundle->build(new ContainerBuilder());
 
         $this->assertInstanceOf(
             EkkoExtension::class,
