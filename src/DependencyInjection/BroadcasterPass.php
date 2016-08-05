98dc606bfdd591a350406d0b3935e4d7d4f98dad
Edwin Luijten <edwin@mediamonks.com>
2016-08-05 16:17:32 +0200

    chunk

--- src/DependencyInjection/BroadcasterPass.php
+++ src/DependencyInjection/BroadcasterPass.php
@@ -4,6 +4,7 @@ namespace EdwinLuijten\EkkoBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 
 class BroadcasterPass implements CompilerPassInterface
@@ -23,31 +24,46 @@ class BroadcasterPass implements CompilerPassInterface
         $taggedServices = $container->findTaggedServiceIds('ekko.broadcaster');
 
         foreach ($taggedServices as $id => $tags) {
-            foreach ($tags as $attributes) {
-                $reference   = new Reference($id);
-                $broadcaster = $container->getDefinition($reference->__toString());
+            $this->createAliasses($id, $tags, $definition, $container);
+        }
+    }
 
-                // Set default broadcaster if defined
-                if (isset($attributes['default'])) {
-                    if (!$container->hasDefinition('ekko.broadcaster.default')) {
-                        $definition->addMethodCall('setDefaultBroadcaster', [$broadcaster]);
-                        $container->setAlias('ekko.broadcaster.default', $reference->__toString());
-                    }
-                }
+    private function createAliasses($id, $tags, Definition $definition, ContainerBuilder $container)
+    {
+        foreach ($tags as $attributes) {
+            $reference   = new Reference($id);
+            $broadcaster = $container->getDefinition($reference->__toString());
 
-                $definition->addMethodCall(
-                    'add',
-                    [
-                        $attributes['alias'],
-                        $broadcaster
-                    ]
-                );
-
-                // Register an alias for each broadcaster
-                if (!$container->hasDefinition('ekko.broadcaster.' . $attributes['alias'])) {
-                    $container->setAlias('ekko.broadcaster.' . $attributes['alias'], $reference->__toString());
-                }
+            // Set default broadcaster if defined
+            if (isset($attributes['default'])) {
+                $this->createDefault($broadcaster, $reference, $definition, $container);
             }
+
+            $this->addBroadcaster($broadcaster, $attributes, $reference, $definition, $container);
+        }
+    }
+
+    private function createDefault($broadcaster, Reference $reference, Definition $definition, ContainerBuilder $container)
+    {
+        if (!$container->hasDefinition('ekko.broadcaster.default')) {
+            $definition->addMethodCall('setDefaultBroadcaster', [$broadcaster]);
+            $container->setAlias('ekko.broadcaster.default', $reference->__toString());
+        }
+    }
+
+    private function addBroadcaster($broadcaster, array $attributes, Reference $reference, Definition $definition, ContainerBuilder $container)
+    {
+        $definition->addMethodCall(
+            'add',
+            [
+                $attributes['alias'],
+                $broadcaster
+            ]
+        );
+
+        // Register an alias for each broadcaster
+        if (!$container->hasDefinition('ekko.broadcaster.' . $attributes['alias'])) {
+            $container->setAlias('ekko.broadcaster.' . $attributes['alias'], $reference->__toString());
         }
     }
 }
